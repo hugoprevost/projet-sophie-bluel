400b48d41ae79fd366e04615130b8d07317dd9fa
Hugo Prevost <43208649+hugoprevost@users.noreply.github.com>
2023-12-11 12:25:30 +0100

    Ajout de la fonctionnalité filtre

--- FrontEnd/javaScript/projet.js
+++ FrontEnd/javaScript/projet.js
@@ -25,4 +25,68 @@ fetch('http://localhost:5678/api/works')
       blockImage.appendChild(projet);
     });
   });
+
+
+// gestion des filtres
+
+// bouton tous
+function btnTous(){
+  //Display Objects//
+  const boutonTous = document.querySelectorAll('div.gallery figure');
+  boutonTous.forEach((element) => {
+    const categoryId = element.getAttribute('category-id');
+      element.style.display = 'block';
+  });
+}
+var bouton = document.getElementById('tous');
+bouton.addEventListener('click',btnTous);
+
+// bouton objets
+function btnObjets(){
+  //Display Objects//
+  const boutonObjets = document.querySelectorAll('div.gallery figure');
+  boutonObjets.forEach((element) => {
+    const categoryId = element.getAttribute('category-id');
+    if (categoryId === '1') {
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
+    }
+  });
+}
+var bouton = document.getElementById('objets');
+bouton.addEventListener('click',btnObjets);
+
+// bouton appartements
+function btnAppartements(){
+  //Display Objects//
+  const boutonAppartements = document.querySelectorAll('div.gallery figure');
+  boutonAppartements.forEach((element) => {
+    const categoryId = element.getAttribute('category-id');
+    if (categoryId === '2') {
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
+    }
+  });
+}
+var bouton = document.getElementById('appartements');
+bouton.addEventListener('click',btnAppartements);
+
+// bouton hotel
+function btnHotels(){
+  //Display Objects//
+  const boutonHotel = document.querySelectorAll('div.gallery figure');
+  boutonHotel.forEach((element) => {
+    const categoryId = element.getAttribute('category-id');
+    if (categoryId === '3') {
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
+    }
+  });
+}
+var bouton = document.getElementById('hotels');
+bouton.addEventListener('click',btnHotels);
+
     
